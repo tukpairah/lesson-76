9843c816a73c6983b37c184ba25d9fef3f739dd1
tukpairah <136407516+tukpairah@users.noreply.github.com>
2023-12-11 21:47:59 +0500

    Add files via upload

---
+++ lesson 76/main.js
@@ -0,0 +1,15 @@
+const firstName = 'Rakhat'
+const lastName = 'Tukpai'
+const age = '17'
+const country = 'Kazakhstan'
+const town = 'Aktobe'
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(country)
+console.log(town)
+
+const x = 10
+const y = 20
+console.log(x + y)
